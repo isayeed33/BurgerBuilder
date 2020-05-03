9745f3bede8778e7519985b07468b18acb6fc37d
Irfan <isayeed33@gmail.com>
2020-05-03 14:25:58 +0530

    Updates

---
+++ precache-manifest.01295141358714e3eb262e027c831250.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12e30ac7eaa52c942da3c563f55ea34",
+    "url": "/BurgerBuilder/index.html"
+  },
+  {
+    "revision": "c36665fd50b649bc7310",
+    "url": "/BurgerBuilder/static/css/main.3f113f05.chunk.css"
+  },
+  {
+    "revision": "309a32fba6533ac313f2",
+    "url": "/BurgerBuilder/static/js/2.ffae3213.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BurgerBuilder/static/js/2.ffae3213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c36665fd50b649bc7310",
+    "url": "/BurgerBuilder/static/js/main.f4591f6a.chunk.js"
+  },
+  {
+    "revision": "4feec09fbfb17e83b6dd",
+    "url": "/BurgerBuilder/static/js/runtime-main.0041ce0d.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilder/static/media/logo.b8503d26.png"
+  }
+]);
